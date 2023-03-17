6d82a13641f2b8a36a9f37c5f37379fb0006b4ce
Corentin Faget <faget.corentin@gmail.com>
2023-03-17 16:28:56 +0100

    [STYLE] register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FileUpload } from '../../models/file-upload.model';
 import { AuthenticationService } from '../../shared/services/authentication.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
+  imports: [RouterLink],
   selector: 'tp-movies-register',
   standalone: true,
   styleUrls: ['./register.component.scss'],
@@ -11,13 +13,13 @@ import { AuthenticationService } from '../../shared/services/authentication.serv
 export class RegisterComponent {
   constructor(private authenticationService: AuthenticationService) {}
 
-  register(email: string, password: string, username: string, file: FileList | null) {
+  register(
+    email: string,
+    password: string,
+    username: string,
+    file: FileList | null,
+  ) {
     const currentFile = new FileUpload(file && file[0]);
-    this.authenticationService.signUp(
-      email,
-      password,
-      username,
-      currentFile,
-    );
+    this.authenticationService.signUp(email, password, username, currentFile);
   }
 }
