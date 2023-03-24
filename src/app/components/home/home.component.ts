e6b717d02e02e2bd640fd87195a9625066aa7259
edouardLaffont <edouard.laffont@live.com>
2023-03-24 16:29:50 +0100

    [ADD]: user display wiith userEffect persistance

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, catchError, take } from 'rxjs';
+import { selectUserInfo } from 'src/app/ngrx/user/user.reducer';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 import { LikesService } from 'src/app/shared/services/likes.service';
-import { UsersService } from 'src/app/shared/services/users.service';
+import { User, UsersService } from 'src/app/shared/services/users.service';
 import { Like, Movie, Trending } from 'src/types/Movies';
 
 @Component({
@@ -26,34 +27,28 @@ export class HomeComponent implements OnInit {
   id$ = this.route.queryParams;
   page = 1;
   movies$: Observable<Trending> | null = null;
-  userId: string | null = null;
-  username: string | null = null;
+  user: User | null = null;
 
   ngOnInit(): void {
-    this.authService.user$.subscribe((user) => {
-      this.usersService
-        .getUserInfoById(<string>user?.uid)
-        .subscribe((userInfo) => {
-          this.userId = userInfo[0].userId;
-          this.username = userInfo[0].username;
-          if (this.userId) {
-            this.likesService.getLikesByUserId(this.userId).subscribe((likes: Like[]) => {
-              this.likes = likes
-            }, take(1))
-          }
-        });
+    this.store.select(selectUserInfo).subscribe((user) => {
+      this.user = user;
+      if (this.user) {
+        this.likesService.getLikesByUserId(this.user.userId).subscribe((likes: Like[]) => {
+          this.likes = likes
+        }, take(1))
+      }
     });
     this.arrivalTime = new Date();
   }
 
   likeMovie(movie: Movie) {
-    if (this.userId !== null && this.username !== null) 
+    if (this.user !== null) 
     {
       const newLike: Like = {
         movieId: movie.id,
         movieTitle: movie.original_title,
-        userId: this.userId,
-        username: this.username,
+        userId: this.user.userId,
+        username: this.user.username,
         createdAt: new Date(),
       }
       this.likesService.likeMovie(newLike).subscribe(() =>  {
@@ -63,15 +58,15 @@ export class HomeComponent implements OnInit {
   }
 
   unlikeMovie(movie: Movie) {
-    if (this.userId !== null) {
-      this.likesService.unlikeMovie(movie.id, this.userId).subscribe(() => {
+    if (this.user !== null) {
+      this.likesService.unlikeMovie(movie.id, this.user.userId).subscribe(() => {
         this.likes = this.likes.filter(like => like.movieId !== movie.id);
       }, take(1));
     }
   }
 
   isLiked(movie: Movie) {
-     return this.likes.some(like => like.movieId === movie.id && like.userId === this.userId);
+     return this.likes.some(like => like.movieId === movie.id && like.userId === this.user?.userId);
   }
 
   toggleSidebar() {
@@ -102,7 +97,7 @@ export class HomeComponent implements OnInit {
     private readonly renderer: Renderer2,
     private readonly likesService: LikesService,
     private readonly route: ActivatedRoute,
-    private readonly store: Store,
+    private readonly store: Store<User>,
     private readonly authService: AuthenticationService,
     private readonly usersService: UsersService,
   ) {
